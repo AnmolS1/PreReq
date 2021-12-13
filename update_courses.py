4d061a7e216a8f3d5a0f35d7108bba79e4be4543
Anmol Saxena <anmolsaxena456@gmail.com>
2021-12-12 20:23:34 -0600

    update python file
    added descriptive comments, also made a few efficiency/readability updates. removed necessity for temporary variable.

--- update_courses.py
+++ update_courses.py
@@ -1,38 +1,52 @@
-import httplib2
-from bs4 import BeautifulSoup
-import re
+# library imports
+import httplib2               # get information from the website
+from bs4 import BeautifulSoup # get specific pieces of the html
+import re                     # regex to make our job easier
 
+# open the file we're writing to with read and write privileges
 file = open('courses.txt', 'w+')
 
+# get the html from the website as a tuple
+# first value is website status, second is the response as a string
 http = httplib2.Http()
 status, response = http.request('https://catalog.ucsd.edu/courses/CSE.html')
 
+# set up some pre-code necessities, the parser using the string
+# the regex to remove tags and stuff in parentheses
+# and "Prerequisites: " to make our code easier to read
 soup = BeautifulSoup(response, 'html.parser')
 remover = re.compile('<.*?>|\(.*?\)')
 prereq_string = 'Prerequisites: '
 
+# get all the course names and descriptions
 course_names = soup(class_='course-name')
 course_descs = soup(class_='course-descriptions')
 
-descriptions = []
-
+# loop through everything
 for i in range(len(course_names)):
+    # get the description and remove any tags we don't wanna see
     course_descs[i] = re.sub(remover, '', str(course_descs[i]))
-    description = course_descs[i]
     
-    temp = course_descs[i].split(prereq_string)
+    # split by 'Prerequisites: '
+    desc = course_descs[i].split(prereq_string)
     
-    if len(temp) == 1:
-        descriptions.append([course_descs[i].strip(), "None."])
+    # if the 'Prerequisites: ' string isn't there then put it in because
+    # that'll make the javascript parsing of the file easier for us
+    # otherwise capitalize the first word of the prerequisites and smush
+    # them back together
+    if len(desc) == 1:
+        desc = desc[0].strip() + ' ' + prereq_string + 'None.'
     else:
-        temp[1] = temp[1][0:1].capitalize() + temp[1][1:]
-        descriptions.append([temp[0].strip(), temp[1].strip()])
+        desc[1] = desc[1][0:1].capitalize() + desc[1][1:]
+        desc = desc[0].strip() + ' ' + prereq_string + desc[1].strip()
     
-    temp = re.sub(remover, '', str(course_names[i])).strip().split(". ")
+    # remove tags/credits from course names. also if the course name has more
+    # than one name (i.e. 'CSE 282/BENG 202') then replace the slash with
+    # the equal sign, again makes the javscript much simpler
+    name = re.sub(remover, '', str(course_names[i])).strip().split(". ")
+    name = name[0].replace("/", " = ") + " = " + name[1]
     
-    name = temp[0].replace("/", " = ") + " = " + temp[1]
-    
-    desc = descriptions[i][0] + " Prerequisites: " + descriptions[i][1]
+    # write to the file :)
     file.write (name + "\n" + desc + "\n")
 
 file.close()
